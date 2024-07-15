b59bc4395b4dab381d4103db326792488fb89426
Pavel333333 <88947332+Pavel333333@users.noreply.github.com>
2024-07-15 16:13:24 +0300

    Update MyException.java

--- src/MyException.java
+++ src/MyException.java
@@ -1,7 +1,10 @@
 class CustomException extends Exception {
+    public CustomException(String message) {
+        super(String.valueOf(message));
+    }
+
     public CustomException(boolean message) {
         super(String.valueOf(message));
     }
 
 }
-
